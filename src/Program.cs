e6082ca6b14b955dd8d6e4e2da82f197ad2ae4d3
Ali Eren Kayhan <a.erenkayhan@gmail.com>
2024-08-30 22:47:44 +0300

    positive character groups

--- src/Program.cs
+++ src/Program.cs
@@ -1,24 +1,12 @@
-using System;
-using System.IO;
 using System.Text.RegularExpressions;
 static bool MatchPattern(string inputLine, string pattern)
 {
-    if (pattern == @"\w")
-    {
-        return Regex.IsMatch(inputLine, @"\w");
-    }
-    else if (pattern == @"\d")
-    {
-        return Regex.IsMatch(inputLine, @"\d");
-    }
-    else if (pattern.Length == 1)
-    {
-        return inputLine.Contains(pattern);
-    }
-    else
-    {
-        throw new ArgumentException($"Unhandled pattern: {pattern}");
-    }
+    if (pattern[0] == '[' && pattern[^1] == ']') return inputLine.AsSpan().ContainsAny(pattern.AsSpan(1, pattern.Length - 2));
+    else if (pattern == @"\w") return Regex.IsMatch(inputLine, @"\w");
+    else if (pattern == @"\d") return Regex.IsMatch(inputLine, @"\d");   
+    else if (pattern.Length == 1) return inputLine.Contains(pattern);
+    else throw new ArgumentException($"Unhandled pattern: {pattern}");
+    
 }
 
 if (args[0] != "-E")
@@ -30,12 +18,5 @@ if (args[0] != "-E")
 string pattern = args[1];
 string inputLine = Console.In.ReadToEnd();
 
-
-if (MatchPattern(inputLine, pattern))
-{
-    Environment.Exit(0);
-}
-else
-{
-    Environment.Exit(1);
-}
+if (MatchPattern(inputLine, pattern))   Environment.Exit(0);
+else  Environment.Exit(1);
